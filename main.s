eb7e2d99d529409839f33d6eb2f9744c9915e6e4
irpo <irpo@irpo.me>
2023-02-16 10:05:11 -0600

    started coding

--- main.s
+++ main.s
@@ -1,1 +1,17 @@
-; vmu stuff
+;;
+;; main.s
+;;
+;; version 0.0.1
+;;
+;; by <Eatcomics>
+;;
+;; No notes currently
+;;
+
+.include "sfr.i"
+
+;; Game Variables
+pet_x = $30	; X position of the pet
+pet_y = $31	; Y position of the pet
+gotbtns = $36	; Buttons currently being pressed
+time = $37	; Time for animation of vpet sprite
