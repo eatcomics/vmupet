10edeb77051ef86b7f38fe669d5c7564c2067653
ironpotato <irpo@irpo.me>
2023-02-16 22:10:43 -0600

    more codez

--- main.s
+++ main.s
@@ -3,15 +3,87 @@
 ;;
 ;; version 0.0.1
 ;;
-;; by <Eatcomics>
+;; by <eatcomics>
 ;;
 ;; No notes currently
 ;;
 
-.include "sfr.i"
+.include "sfr.s"
 
 ;; Game Variables
-pet_x = $30	; X position of the pet
-pet_y = $31	; Y position of the pet
+pet_x	= $30	; X position of the pet
+pet_y	= $31	; Y position of the pet
 gotbtns = $36	; Buttons currently being pressed
-time = $37	; Time for animation of vpet sprite
+time	= $37	; Time for animation of vpet sprite
+
+b_sleep = $7	; Sleep button position
+b_mode	= $6	; Mode button position
+b_b	= $5	; B
+b_a	= $4	; A
+b_r	= $3	; Right
+b_l	= $2	; Left
+b_d	= $1	; Down
+b_u	= $0	; Up
+
+;; Reset and interrupt vectors
+.org $00	; Reset Vector
+jmpff __main	; Jump to vmupet entry point
+
+.org $03	; INT0 (external)
+jmp __nop_vec
+
+.org $0B	; INT1 (external)
+jmp __nop_vec
+
+.org $13	; INT2 (external) or T0L overflow
+jmp __nop_vec
+
+.org $1B	; INT3 (external or base timer overflow
+jmp __nop_vec
+
+.org $23	; T0H overflow
+jmp __nop_vec
+
+.org $2B	; T1H or T1L overflow
+jmp __nop_vec
+
+.org $33	; SIO0
+jmp __nop_vec
+
+.org $3B	; SIO1
+jmp __nop_vec
+
+.org $43	; RFB
+jmp __nop_vec
+
+.org $4B	; P3
+jmp __nop_vec
+
+__nop_vec:
+    reti	; Return, used for unnecessary interrupts (ISRs)
+
+    .org $130	; Firmware entry vector - Update system time
+__time_vec:
+    push ie
+    clr1 ie, 7
+    not1 ext, 0
+    jmpf __time_vec
+    pop ie
+    reti
+
+    .org $1F0	; Frimware entry vector - Leave game mode
+__goodbye:
+    not1 ext, 0
+    jmpf __goodbye
+
+;; VMS File Header
+    .org $200			; Header starts at $200 for games
+    .text 16 "VMUPET"		; 16 bytes for file description
+    .text 32 "VMUPET FOR VMU"	; 32 bytes of file description (for dreamcast)
+    .string 16 "" 		; Identifier of application that created the file (we don't need it)
+
+    .include icon "./assets/icon.png" ; waterbear handles this nicely, nice clean code for us!
+
+;; Finally our code! Main entry point
+    .org $680	; Main starts at $680
+__main:
---
+++ sfr.s
@@ -0,0 +1,64 @@
+; -----------------------------------------------------------------------------
+; VMS Special Function Register Address Constants
+;
+;
+; Author:    Shirobon
+; Date:      2023/02/11
+; Credit:    Marcus Comstedt
+; Reference: https://mc.pp.se/dc/vms/sfr.html
+; -----------------------------------------------------------------------------
+
+acc     equ $100
+psw     equ $101
+b       equ $102
+c       equ $103
+trl     equ $104
+trh     equ $105
+sp      equ $106
+pcon    equ $107
+ie      equ $108
+ip      equ $109
+ext     equ $10d
+ocr     equ $10e
+t0con   equ $110
+t0prr   equ $111
+t0l     equ $112
+t0lr    equ $113
+t0h     equ $114
+t0hr    equ $115
+t1cnt   equ $118
+t1lc    equ $11a
+t1l     equ $11b
+t1lr    equ $11b
+t1hc    equ $11c
+t1h     equ $11d
+t1hr    equ $11d
+mcr     equ $120
+stad    equ $122
+cnr     equ $123
+tdr     equ $124
+xbnk    equ $125
+vccr    equ $127
+scon0   equ $130
+sbuf0   equ $131
+sbr     equ $132
+scon1   equ $134
+sbuf1   equ $135
+p1      equ $144
+p1ddr   equ $145
+p1fcr   equ $146
+p3      equ $14c
+p3ddr   equ $14d
+p3int   equ $14e
+p7      equ $15c
+i01cr   equ $15d
+i23cr   equ $15e
+isl     equ $15f
+vsel    equ $163
+vrmad1  equ $164
+vrmad2  equ $165
+vtrbf   equ $166
+vlreg   equ $167
+btcr    equ $17f
+xram    equ $180
+
