0cff50e8b4e00d2508e71372b8b43d91ac9679a3
ironpotato <irpo@irpo.me>
2023-02-19 14:25:54 -0600

    drawing works, and added art

--- main.s
+++ main.s
@@ -8,9 +8,13 @@
 ;; No notes currently
 ;;
 
-.include "sfr.i"
+.include "lib/sfr.i"
 
 ;; Game Variables
+v_btn		= $10	; Current active state of buttons
+v_btn_old	= $11	; Previous state of buttons
+v_btn_chg	= $12	; Which buttons have chnaged from v_btn_old to v_btn
+
 pet_x		= $30	; X position of the pet			1 byte
 pet_y		= $31	; Y position of the pet			1 byte
 pet_width	= $32	; Width of pet sprite			1 byte
@@ -96,7 +100,7 @@ __main:
     mov #$a1, ocr	; Set up OCR, I don't know if this is what I want or not REVIEW THIS LATER
     mov #$09, mcr	; Set up Mode Control Register
     mov #$80, vccr	; Set up LCD Contrast Control Register
-    clr1 p3int, 0	; Cear bit 0 in p3int - For interrupts on button press
+    clr1 p3int, 0	; Clear bit 0 in p3int - For interrupts on button press
     clr1 p1, 7		; Sets the sound output port
     mov #$FF, p3	; p3 are buttons - 0 for pressed 1 for unpressed
 
@@ -116,33 +120,69 @@ __main:
 
     call __pollbuttons	; Get the initial button state
 
+    ;;; This is initing variables for the Pet Sprite, should probably put this somewhere else...
+    ld 		pet_x		; Load accumulator with pet x
+    st 		b		; Store in b (for drawing)
+    mov 	#20, acc	; Sprite width is 32
+    mov 	#<pet_spr, acc
+    st		trl
+    st		pet_spr_addr
+    mov 	#>pet_spr, acc
+    st		trh
+    st		pet_spr_addr+1
+    xor		acc
+    ldc
+    st		pet_width
+    mov 	#1, acc
+    ldc
+    st		pet_height
+
 .main
     ;; Here is where we'll draw the title screen, then we'll wait for a button press and jump to the real game loop
-    mov #<title1, trl
-    mov #>title1, trh
+    mov			#<title1, trl
+    mov			#>title1, trh
     inc trl
     inc trl
-    call __copytovf
-    call __commitvf
 
 .wait_for_start:
     call __pollbuttons
-    bn v_btn, b_a, .wati_for_start
+    bn v_btn, b_a, .wait_for_start
 
-    mv #18, vpet_x
-    mv #24, vpet_y
+    mov #0, pet_x
+    mov #0, pet_y
 
 .game_loop
     ;;;;; Here there will be a bunch of logic and shiz for the animations, but for now we'll just draw the pet sprite
     call __pollbuttons
 
 .game_logic:
-    call __clearvf	; Clear the virtual framebuffer
     call __drawpet	; Draw the pet to the virtual framebuffer
-    call __commitvf	; Actually commit sprites to virtual framebuffer
 
 __drawpet:
-    ld vpet_x		; Load accumulator with pet x
-    st b		; Store in b (for drawing)
-    mov #10, acc	; This has to do with the sprite width (I dunno yet, but we'll store in acc as a counter)
-    .include sprite "assets/pet.png"	; Waterbear is cool and will import sprites in the format Kresna's LibPerspective uses
+    P_Draw_Sprite 	pet_spr_addr, pet_x, pet_y
+    P_Blit_Screen
+    
+__pollButtons:
+    bp p7, 0, .quit	; When the VMU is plugged into controller, this bit goes high
+    push acc		; Save acc so we can use it for reading and storing
+    ld v_btn		; The current set of buttons is now the old set
+    st v_btn_old
+    ld p3		; Read value of port 3 (buttons)
+    bn acc, 6, .quit	; Bit 6 is the mode button, it will quit
+    xor #$FF		; Invert button state, so 1=Pressed 0=Unpressed
+    st v_btn		; The current set of buttons is now the new set just read
+    xor v_btn_old	; XOR the new set with the old set, this will give us changed buttons
+    st v_btn_chg
+    pop acc
+    ret
+.quit:			; When the VMU is plugged in to controller quit the game
+    jmp __goodbye
+
+.include "lib/libperspective.asm"	; Kresna's lib perspective for fancy sprite drawing macros
+;.include "lib/libkcommon.asm"		; libperspective needs this I think
+title1:
+	.include sprite "assets/title1.png"
+pet_spr: 
+	.include sprite "assets/pet.png"	; Waterbear is cool and will import sprites in the format Kresna's LibPerspective uses
+
+
